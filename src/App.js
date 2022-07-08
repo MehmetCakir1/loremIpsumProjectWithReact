b2e0910f9d392dd19a9cbc0e1837d40238778f0b
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-07-08 21:27:20 +0300

    lorem ipsum project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import text from "./helper/data";
+import "./App.css"
 
-function App() {
+const App = () => {
+  
+  const [number, setNumber] = useState(1);
+  const [data, setData] = useState([]);
+  const [show,setShow]=useState(true);
+  
+  const handleChange = (e) => { 
+    setNumber(e.target.value);
+    // console.log(e.target.value);
+  };
+  console.log(number);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(number>=1){
+      setData(text.slice(0,number));
+      setShow(false)
+    }else{
+      setData([])
+      alert("Please enter a number greater than 1")
+    }
+  };
+  // console.log(text.length);
+  // console.log(data[0]);
+  // console.log(data.slice(0,number))
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <h1 className="header">TIRED OF BORING LOREM IPSUM?</h1>
+      <form className="form">
+        <label htmlFor="number">Paragraphs: </label>
+        <input
+          onChange={handleChange}
+          type="number"
+          id="number"
+          name="number"
+          min="1"
+          value={number}
+        />
+        <button onClick={handleSubmit} type="submit">
+          GENERATE
+        </button>
+      </form>
+      {show ? (
+        <article>
+          <div className="list">
+              <p >{text[1]}</p>
+            </div>
+      </article>
+      ):
+        (<article>
+        {data.map((item,index) => {
+          return (
+            <div className="list" key={index}>
+              <p >{item}</p>
+            </div>
+          );
+        })}
+      </article>)
+      }
+      
     </div>
   );
-}
+};
 
 export default App;
